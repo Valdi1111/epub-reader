e943cd008dc9726d642d9268bd85c1a9b2e1bcbc
Davide Valdifiori <davide.valdifiori.dv@gmail.com>
2022-12-10 14:01:24 +0100

    Edit sub shelf visualization, now using accordion.

--- frontend/src/components/library/shelves/ShelfContent.js
+++ frontend/src/components/library/shelves/ShelfContent.js
@@ -5,22 +5,26 @@ function ShelfContent(props) {
 
     function Section(props) {
         const {uid, shelf, books} = props;
+        const headId = "section-header-" + uid;
+        const bodyId = "section-body-" + uid;
         return (
-            <>
-                <div className={"p-2"}>
-                    <button className={"btn btn-outline-secondary w-100"} type={"button"} data-bs-toggle={"collapse"}
-                            data-bs-target={`#${uid}`} aria-expanded={true} aria-controls={uid}>
+            <div className={"accordion-item"}>
+                <h2 id={headId} className={"accordion-header"}>
+                    <button className={"accordion-button"} type={"button"} data-bs-toggle={"collapse"}
+                            data-bs-target={`#${bodyId}`} aria-expanded={true} aria-controls={bodyId}>
                         {shelf}
                     </button>
+                </h2>
+                <div id={bodyId} className={"accordion-collapse collapse show"} aria-labelledby={headId}>
+                    <div className={"accordion-body row mx-0 p-0"}>
+                        {books.map(b =>
+                            <div className={"col-auto p-2"} key={b.id}>
+                                <LibraryItem book={b}/>
+                            </div>
+                        )}
+                    </div>
                 </div>
-                <div id={uid} className={"collapse row mx-0 show"}>
-                    {books.map(b =>
-                        <div className={"col-auto p-2"} key={b.id}>
-                            <LibraryItem book={b}/>
-                        </div>
-                    )}
-                </div>
-            </>
+            </div>
         );
     }
 
@@ -29,7 +33,7 @@ function ShelfContent(props) {
         let num = 0;
         for (let key in elements) {
             let val = elements[key];
-            e = [...e, <Section key={key} uid={`shelf-section-${num}`} shelf={key} books={val}/>];
+            e = [...e, <Section key={key} uid={num} shelf={key} books={val}/>];
             num++;
         }
         return e;
@@ -37,7 +41,7 @@ function ShelfContent(props) {
 
     return (
         <div className={`d-md-block scroll-pane col-12 col-md-8 col-lg-9 ${id === undefined ? "d-none" : ""}`}>
-            <div className={"scroll-pane-inner"}>
+            <div id={"sections-accordion"} className={"scroll-pane-inner accordion accordion-flush"}>
                 {getSections()}
             </div>
         </div>
